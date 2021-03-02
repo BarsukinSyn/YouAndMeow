5de69cee3313f7dfdbcb03df412ef2f280dec344
Vladimir <v.asankin@yandex.ru>
2021-03-02 11:42:00 +0300

    feat: system audio engine extension added

---
+++ YouAndMeow/Platform/Extensions/AVAudioEngine.swift
@@ -0,0 +1,40 @@
+//
+//  AVAudioEngine.swift
+//  YouAndMeow
+//
+//  Created by Vladimir on 20.06.2020.
+//  Copyright © 2020 Barsukin syn. All rights reserved.
+//
+
+import AVFoundation
+
+extension AVAudioEngine {
+  func attach(_ nodes: [AVAudioNode]) {
+    nodes.forEach { self.attach($0) }
+  }
+
+  func attach(_ nodes: AVAudioNode...) {
+    self.attach(nodes)
+  }
+
+  func connect(_ nodes: [AVAudioNode], format: AVAudioFormat?) {
+    for x in 0 ..< nodes.count - 1 {
+      self.connect(nodes[x], to: nodes[x + 1], format: format)
+    }
+  }
+
+  func connect(_ nodes: AVAudioNode..., format: AVAudioFormat?) {
+    self.connect(nodes, format: format)
+  }
+
+  func disconnect(_ nodes: [AVAudioNode]) {
+    nodes.forEach {
+      self.disconnectNodeInput($0)
+      self.disconnectNodeOutput($0)
+    }
+  }
+
+  func disconnect(_ nodes: AVAudioNode...) {
+    self.disconnect(nodes)
+  }
+}
