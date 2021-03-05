937f84b01e7f9db1233ab4006c367e9331a0dc57
Vladimir <v.asankin@yandex.ru>
2021-03-05 09:59:00 +0300

    feat: a data decoder added

---
+++ YouAndMeow/Platform/FileSystem/DataDecoder.swift
@@ -0,0 +1,37 @@
+//
+//  DataDecoder.swift
+//  YouAndMeow
+//
+//  Created by Vladimir on 24.06.2020.
+//  Copyright © 2020 Barsukin syn. All rights reserved.
+//
+
+import Foundation
+
+class DataDecoder {
+  static func decodePropertyList<T: Decodable>(_ data: Data) throws -> T {
+    do {
+      let decodedData = try PropertyListDecoder().decode(T.self, from: data)
+
+      return decodedData
+    } catch {
+      throw DataDecoder.Exception.unexpectedDataType
+    }
+  }
+
+  static func decodeJSON<T: Decodable>(_ data: Data) throws -> T {
+    do {
+      let decodedData = try JSONDecoder().decode(T.self, from: data)
+
+      return decodedData
+    } catch {
+      throw DataDecoder.Exception.unexpectedDataType
+    }
+  }
+}
+
+extension DataDecoder {
+  enum Exception: Error {
+    case unexpectedDataType
+  }
+}
