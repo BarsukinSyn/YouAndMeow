88d3e301e86e9c9ed4c683e5cfeb3018120e76a8
Vladimir <v.asankin@yandex.ru>
2021-03-09 13:14:00 +0300

    feat: wrapper for playback setting added

---
+++ YouAndMeow/Entities/PlaybackSettings/PlaybackSetting.swift
@@ -0,0 +1,36 @@
+//
+//  PlaybackSetting.swift
+//  YouAndMeow
+//
+//  Created by Vladimir on 30.06.2020.
+//  Copyright © 2020 Barsukin syn. All rights reserved.
+//
+
+import Foundation
+
+@propertyWrapper
+struct PlaybackSetting {
+  let minimumValue: Float
+  let maximumValue: Float
+
+  var projectedValue: Float {
+    self.currentValue
+  }
+
+  var wrappedValue: Float {
+    get { self.currentValue }
+    set { self.currentValue = (self.minimumValue...self.maximumValue).clamped(newValue) }
+  }
+
+  var averageValue: Float {
+    (self.minimumValue + self.maximumValue) / 2
+  }
+
+  private var currentValue: Float = 0
+
+  init(wrappedValue: Float, minimumValue: Float, maximumValue: Float) {
+    self.minimumValue = minimumValue
+    self.maximumValue = maximumValue
+    self.wrappedValue = wrappedValue
+  }
+}
